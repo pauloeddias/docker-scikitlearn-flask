d9b417e45f4080448e02835cd9bc88e85202747a
Paulo Silva <paulo.silva@crmeducacional.com>
2020-02-11 10:03:19 -0300

    corrections

---
+++ train/train.py
@@ -0,0 +1,18 @@
+import numpy as np
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+
+n=1000
+x = np.linspace(0,10,n)
+y = 0.5*x+2
+noise = np.random.normal(0,0.1,n)
+y = y + noise
+
+x = x.reshape(-1,1)
+y = y.reshape(-1,1)
+model = LinearRegression()
+model.fit(x,y)
+yhat = model.predict(x)
+
+import joblib
+joblib.dump(model,'model.joblib')
